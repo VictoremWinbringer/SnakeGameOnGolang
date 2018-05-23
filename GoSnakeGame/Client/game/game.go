a045a892fde868237133de25ec636fbb26f2aa64
=VictoremWinbringer <V@mail.ru>
2018-05-23 22:07:20 +0300

    change user input

--- GoSnakeGame/Client/game/game.go
+++ GoSnakeGame/Client/game/game.go
@@ -9,20 +9,29 @@ import (
 	tcellModule "github.com/gdamore/tcell"
 )
 
-type Game interface {
+type Command uint8
+
+const Left Command = 1
+const Right Command = 2
+const Up Command = 3
+const Down Command = 4
+const Exit Command = 5
+
+type IGame interface {
 	Draw()
 	Logic(timeDeltaInNanoSeconds int64) bool
 }
 
 type game struct {
-	frame      frameModule.Frame
-	food       foodModule.Food
-	snake      snakeModule.Snake
-	screen     tcellModule.Screen
-	timeBuffer int64
+	frame          *frameModule.Frame
+	food           *foodModule.Food
+	snake          *snakeModule.Snake
+	screen         tcellModule.Screen
+	timeBuffer     int64
+	commandChannel chan Command
 }
 
-func New(height int, width int) (Game, error) {
+func New(height int, width int) (IGame, error) {
 	screen, e := tcellModule.NewScreen()
 	if e != nil {
 		return nil, e
@@ -36,7 +45,7 @@ func New(height int, width int) (Game, error) {
 	frame := frameModule.New(height, width, '+', writer)
 	food := foodModule.New(10, 10, width, height, '$', writer)
 	snake := snakeModule.New(8, 8, '+', writer)
-	return &game{frame, food, snake, screen, 0}, nil
+	return &game{&frame, &food, &snake, screen, 0, keyboardInput(screen)}, nil
 }
 
 func (game *game) Draw() {
@@ -44,35 +53,59 @@ func (game *game) Draw() {
 	game.screen.Clear()
 	game.frame.Draw()
 	game.food.Draw()
-	game.snake.Move()
 	game.snake.Draw()
 	game.screen.Show()
 }
 
 const timeDeltaInNanoSecondsAfterThatSnakeMoves int64 = 100000000
 
+func keyboardInput(screen tcellModule.Screen) chan Command {
+	commandChannel := make(chan Command)
+	go func() {
+		for {
+			event := screen.PollEvent()
+			keyEvent, ok := event.(*tcellModule.EventKey)
+			if ok {
+				switch keyEvent.Key() {
+				case tcellModule.KeyUp:
+					commandChannel <- Up
+				case tcellModule.KeyDown:
+					commandChannel <- Down
+				case tcellModule.KeyLeft:
+					commandChannel <- Left
+				case tcellModule.KeyRight:
+					commandChannel <- Right
+				case tcellModule.KeyEsc:
+					commandChannel <- Exit
+				}
+			}
+		}
+	}()
+	return commandChannel
+}
+
 func (game *game) Logic(timeDeltaInNanoSeconds int64) bool {
 	game.timeBuffer += timeDeltaInNanoSeconds
-	event := game.screen.PollEvent()
-	switch keyEvent := event.(type) {
-	case *tcellModule.EventKey:
-		switch keyEvent.Key() {
-		case tcellModule.KeyEsc:
-			game.screen.Clear()
-			game.screen.Show()
-			game.screen.ShowCursor(0, 0)
-			return false
-		case tcellModule.KeyUp:
+	select {
+	case command := <-game.commandChannel:
+		switch command {
+		case Up:
 			game.snake.Go(snakeModule.Up)
-		case tcellModule.KeyDown:
+		case Down:
 			game.snake.Go(snakeModule.Down)
-		case tcellModule.KeyLeft:
+		case Left:
 			game.snake.Go(snakeModule.Left)
-		case tcellModule.KeyRight:
+		case Right:
 			game.snake.Go(snakeModule.Right)
+		case Exit:
+			game.screen.Clear()
+			game.screen.ShowCursor(0, 0)
+			game.screen.Show()
+			return false
 		}
+	default:
 	}
-	game.snake.TryEat(&game.food)
+	game.snake.TryEat(game.food)
 	if game.snake.IsHit(game.frame.Figure) || game.snake.IsHitTail() {
 		game.snake.Reset()
 	}
