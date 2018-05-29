ab035c84c88971834320aa3f3882ab558a65e16f
=VictoremWinbringer <V@mail.ru>
2018-05-29 22:02:39 +0300

    moving and added test

--- src/Server/serverModule/dal/session.go
+++ src/Server/serverModule/dal/session.go
@@ -4,3 +4,10 @@ type ISession interface {
 	HandleCommand(command int)
 	GetState() [][]rune
 }
+
+type session struct {
+}
+
+func Mul(x, y int) int {
+	return x * y
+}
---
+++ tests/session_test.go
@@ -0,0 +1,22 @@
+package dal
+
+import "testing"
+
+func TestMul(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{{"ad", args{2, 3}, 6}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Mul(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Mul() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
