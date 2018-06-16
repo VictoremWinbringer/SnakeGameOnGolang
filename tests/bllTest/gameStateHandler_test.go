e633f9adef39492d6f501604bacf3b157d57a442
=VictoremWinbringer <V@mail.ru>
2018-06-16 22:19:05 +0300

    save test for gameStateHandler Handle method

--- tests/bllTest/gameStateHandler_test.go
+++ tests/bllTest/gameStateHandler_test.go
@@ -18,3 +18,51 @@ func Test_gameStateHandler_Type(t *testing.T) {
 		}
 	})
 }
+
+func Test_gameStateHandler_Handle(t *testing.T) {
+	want := serializer.GameState{makeMatrix()}
+	handler := bll.NewSeverBllFactory().CreateGameStateHandler()
+	t.Run("test_session_HandleCommand", func(t *testing.T) {
+		gotBytes, ok := handler.Handle(make([]byte, 0), moqSession{})
+		if !ok {
+			t.Error("Not Ok!")
+		}
+		got := serializer.DecodeGameState(gotBytes)
+		if !Equals(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func Equals(x, y serializer.GameState) bool {
+	if len(x.State) != len(y.State) {
+		return false
+	}
+
+	for i, s := range x.State {
+		for j, r := range s {
+			if r != y.State[i][j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+type moqSession struct {
+}
+
+func (this moqSession) GetState() [][]rune {
+	return makeMatrix()
+}
+
+func (this moqSession) HandleCommand(command int) {
+	//Do nothing
+}
+
+func makeMatrix() [][]rune {
+	matrix := make([][]rune, 1)
+	matrix[0] = make([]rune, 1)
+	matrix[0][0] = 1
+	return matrix
+}
